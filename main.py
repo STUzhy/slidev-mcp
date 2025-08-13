d5b4a34384c6fe830a7502b528f1aacccc804fbd
Kirigaya <1193466151@qq.com>
2025-08-13 20:25:19 +0800

    update prompt

--- main.py
+++ main.py
@@ -153,7 +153,7 @@ def transform_parameters_to_frontmatter(parameters: dict):
 
 
 @mcp.prompt()
-def guide_prompt():
+def slidev_generate_prompt():
     """guide the ai to use slidev"""
     return """
 你是一个擅长使用 slidev 进行讲演生成的 agent，如果用户给你输入超链接，你需要调用 websearch 工具来获取对应的文本。对于返回的文本，如果你看到了验证码，网络异常等等代表访问失败的信息，你需要提醒用户本地网络访问受阻，请手动填入需要生成讲演的文本。
@@ -165,7 +165,7 @@ def guide_prompt():
 - academic 主题的 frontmatter: https://raw.githubusercontent.com/alexanderdavide/slidev-theme-academic/refs/heads/master/README.md
 
 遇到 `:` 开头的话语，这是命令，目前的命令有如下的：
-- `:sum {{url}}`: 爬取目标网页内容并整理，如果爬取失败，你需要停下来让用户手动输入网页内容的总结。
+- `:sum {{url}}`: 使用 `websearch` 爬取目标网页内容并整理，如果爬取失败，你需要停下来让用户手动输入网页内容的总结。
 - `:mermaid {{description}}`: 根据 description 生成符合描述的 mermaid 流程图代码，使用 ```mermaid ``` 进行包裹。
 
 如果用户要求你生成大纲或者摘要，那么一定要调用 `slidev_save_outline` 这个函数来保存你总结好的大纲结果。
@@ -173,6 +173,26 @@ def guide_prompt():
 现在请爬取如下链接来获取 academic 基本的使用方法
 https://raw.githubusercontent.com/alexanderdavide/slidev-theme-academic/refs/heads/master/README.md
 """
+
+@mcp.prompt()
+def outline_generate_prompt():
+    """generate outline for slidev"""
+    return """
+你是一个擅长使用 slidev 进行讲演生成的 agent，如果用户让你生成给定素材的大纲，从而在后续生成 slidev，那么你应该先根据用户输入的素材，生成一个大纲。
+生成大纲后，你需要调用 `slidev_save_outline` 来保存这次的结果。
+
+你不被允许在生成大纲时，执行任何关于 slidev 项目生成，创建，修改和添加页面的操作。
+
+如果遇到用户给定的素材中 http 或者 https 链接，你应该积极地使用 `websearch` 来爬去网页内容。
+
+如果遇到 `:` 开头的话语，这是命令，目前的命令有如下的：
+- `:sum {{url}}`: 使用 `websearch` 爬取目标网页内容并整理，如果爬取失败，你需要停下来让用户手动输入网页内容的总结。
+- `:mermaid {{description}}`: 根据 description 生成符合描述的 mermaid 流程图代码，使用 ```mermaid ``` 进行包裹。
+
+---
+
+下面是用户的输入：
+    """
 
 
 @mcp.tool(
